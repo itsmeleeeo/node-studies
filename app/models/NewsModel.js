4349c65d4607b3ca898dc40fe9670ccd57a11621
Leonardo <fsleonardo91@gmail.com>
2024-05-17 20:03:33 -0700

    removed the console log and created  form view

--- app/models/NewsModel.js
+++ app/models/NewsModel.js
@@ -5,7 +5,6 @@ module.exports = () => {
 
     this.getNew = (conn, callback) => {
         conn.query("select * from news where id=2", callback);
-        console.log(callback)
     }
     
     return this;
