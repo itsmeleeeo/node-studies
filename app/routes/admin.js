c8a44f9f10c13b585f199cbbd4ba6507092baca4
Leonardo <fsleonardo91@gmail.com>
2024-05-27 20:02:09 -0700

    validating data and adding few more fields to the form

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,3 +1,5 @@
+const { format } = require("mysql");
+
 module.exports = (app) => {
     app.get("/add_news", (req, res) => {
         res.render("admin/form_add_news");
@@ -5,6 +7,20 @@ module.exports = (app) => {
 
     app.post("/news/save", (req, res) => {
         let newsInfo = req.body;
+
+        req.assert("title","The Title cannot be empty!").notEmpty();
+        req.assert("summary", "Summary cannot be empty!").len(10, 100);
+        req.assert("author", "Author's name must be filled").notEmpty();
+        req.assert("news_date", "The date cannot be blank").notEmpty();
+        req.assert("news", "News cannot be empty").notEmpty();
+
+        let err = req.validationErrors();
+
+        if(err) {
+            res.render("admin/form_add_news", {validate: err});
+            return; 
+        }
+
         let conn = app.config.DbConnection();
         let newsModel = new app.app.models.NewsDAO(conn);
 
